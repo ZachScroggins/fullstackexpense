bc9b5d164c751f1ee518d7859d4dbd4fba2d47b9
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-12-14 13:05:34 -0600

    preflight headers

--- server/app/api/expense/update.php
+++ server/app/api/expense/update.php
@@ -5,6 +5,14 @@ header('Access-Control-Allow-Origin: *');
   header('Access-Control-Allow-Methods: PUT');
   header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods');
 
+  $method = $_SERVER['REQUEST_METHOD'];
+if ($method == "OPTIONS") {
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("HTTP/1.1 200 OK");
+    die();
+}
+
   include_once '../../../config/Database.php';
   include_once '../../../models/Expense.php';
 
